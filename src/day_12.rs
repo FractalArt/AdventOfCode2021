ccd49054fc5187be798d44bc13f4f6e410b318cc
fractalart <FractalArt@users.noreply.github.com>
2021-12-15 23:17:15 +0100

    Day 12 part 1 done.

--- src/day_12.rs
+++ src/day_12.rs
@@ -1,66 +1,52 @@
 //! # Advent of Code 2021 - Day 12
 //!
 //! This module contains the solution of the [twelveth day's challenges](https://adventofcode.com/2021/day/12).
-
 use std::collections::HashSet;
 
-/// The possible nodes that can appear in connections.
-#[derive(Debug, Hash, PartialEq, Eq, Clone)]
-enum Node {
-    Start,
-    End,
-    Intern(char)
-}
-
 /// Extract the possible connections from the input
-fn get_rules(data: &[String]) -> Vec<HashSet<Node>> {
+fn get_rules(data: &[String]) -> Vec<HashSet<&str>> {
     data.iter()
-    .map(|line| match line.split_once("-").unwrap() {
-        ("start", c) => [Node::Start, Node::Intern(c.chars().next().unwrap())].into_iter().collect::<HashSet<Node>>(),
-        (c, "end") => [Node::Intern(c.chars().next().unwrap()), Node::End].into_iter().collect::<HashSet<Node>>(),
-        (c1, c2) => [Node::Intern(c1.chars().next().unwrap()), Node::Intern(c2.chars().next().unwrap())].into_iter().collect::<HashSet<Node>>()
-    }).collect()
+        .map(|line| line.split('-').collect::<HashSet<&str>>())
+        .collect()
 }
-
 
 /// Get the next possible nodes.
-fn get_next_nodes(current_node: &Node, connections: &Vec<HashSet<Node>>) -> Vec<Node> {
-    connections.iter()
-    .filter(|conn| conn.contains(current_node))
-    .map(|conn| conn.iter().find(|&c| c != current_node).unwrap().clone())
-    .collect()
+fn get_next_nodes<'a>(current_node: &str, connections: &[HashSet<&'a str>]) -> Vec<&'a str> {
+    connections
+        .iter()
+        .filter(|conn| conn.contains(current_node))
+        .map(|conn| *conn.iter().find(|&&c| c != current_node).unwrap())
+        .filter(|&node| node != "start")
+        .collect()
 }
 
-
 /// Given the `current_node`, count the paths from there that reach the end.
-fn paths_that_reach_end(current_node: Node, connections: &Vec<HashSet<Node>>, lowercases_visited: HashSet<char>) -> usize {
+fn paths_that_reach_end(
+    current_node: &str,
+    connections: &[HashSet<&str>],
+    lowercases_visited: HashSet<&str>,
+) -> usize {
     // Loop over all next nodes and check if they reach the end, sum
-    let mut sum = 0;
-    let possible_next_nodes = get_next_nodes(&current_node, connections);
-    for node in possible_next_nodes {
-        match node {
-            Node::End => { sum += 1; },
-            Node::Intern(c) if c.is_uppercase() => { sum += paths_that_reach_end(node, connections, lowercases_visited.clone()); },
-            Node::Intern(c) => {
-                if lowercases_visited.contains(&c) {
-                    sum += 0
-                } else {
-                    let mut lowercases_visited = lowercases_visited.clone();
-                    lowercases_visited.insert(c);
-                    sum += paths_that_reach_end(node, connections, lowercases_visited);
-                }
+    get_next_nodes(current_node, connections)
+        .into_iter()
+        .map(|node| match node {
+            "end" => 1,
+            s if s.chars().all(char::is_uppercase) => {
+                paths_that_reach_end(node, connections, lowercases_visited.clone())
             }
-            _ => {}
-        }
-    }
-
-    sum
+            s if !lowercases_visited.contains(s) => {
+                let mut lowercases_visited_copy = lowercases_visited.clone();
+                lowercases_visited_copy.insert(s);
+                paths_that_reach_end(node, connections, lowercases_visited_copy)
+            }
+            _ => 0,
+        })
+        .sum()
 }
 
 /// Find the number of paths between starting and ending points visiting each lowercase cave only once.
 pub fn day_12_1(data: &[String]) -> usize {
-    let connections = get_rules(data);
-    paths_that_reach_end(Node::Start, &connections, HashSet::new())
+    paths_that_reach_end("start", &get_rules(data), HashSet::new())
 }
 
 #[cfg(test)]
@@ -79,6 +65,30 @@ mod tests {
             "A-end".to_string(),
             "b-end".to_string(),
         ];
+
         assert_eq!(day_12_1(&data), 10);
+
+        let data = vec![
+            "fs-end".to_string(),
+            "he-DX".to_string(),
+            "fs-he".to_string(),
+            "start-DX".to_string(),
+            "pj-DX".to_string(),
+            "end-zg".to_string(),
+            "zg-sl".to_string(),
+            "zg-pj".to_string(),
+            "pj-he".to_string(),
+            "RW-he".to_string(),
+            "fs-DX".to_string(),
+            "pj-RW".to_string(),
+            "zg-RW".to_string(),
+            "start-pj".to_string(),
+            "he-WI".to_string(),
+            "zg-he".to_string(),
+            "pj-fs".to_string(),
+            "start-RW".to_string(),
+        ];
+
+        assert_eq!(day_12_1(&data), 226);
     }
 }
---
+++ tests/day_12.rs
@@ -0,0 +1,8 @@
+use aoc2021::{self, read_data};
+
+#[test]
+fn test_day_10() {
+    let data = read_data::<String, _>("data/day12.txt").unwrap();
+    let task_1 = aoc2021::day_12::day_12_1(&data);
+    assert_eq!(task_1, 5212);
+}
