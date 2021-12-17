cd5fe666e7b1756d6d17d4527f15c6d137517728
fractalart <FractalArt@users.noreply.github.com>
2021-12-17 19:26:44 +0100

    Day 14 part 2.

--- src/day_14.rs
+++ src/day_14.rs
@@ -4,12 +4,24 @@
 use itertools::Itertools;
 use std::collections::HashMap;
 
-type InsertionRules = HashMap<(char, char), char>;
+type LetterCounter = HashMap<char, usize>;
+type ConnectionCounter = HashMap<(char, char), usize>;
+type Connections = HashMap<(char, char), char>;
 
-/// Parse the input into the polymer template string and the insertion rules.
-fn parse_input(data: &[String]) -> (String, InsertionRules) {
+/// Parse the input.
+fn parse_input(data: &[String]) -> (LetterCounter, ConnectionCounter, Connections) {
     (
-        data[0].clone(),
+        data[0].chars().fold(HashMap::new(), |mut hm, c| {
+            *hm.entry(c).or_insert(0) += 1;
+            hm
+        }),
+        data[0]
+            .chars()
+            .tuple_windows()
+            .fold(HashMap::new(), |mut hm, (c1, c2)| {
+                *hm.entry((c1, c2)).or_insert(0) += 1;
+                hm
+            }),
         data.iter()
             .skip(2)
             .map(|s| s.split_once(" -> ").unwrap())
@@ -23,36 +35,37 @@ fn parse_input(data: &[String]) -> (String, InsertionRules) {
     )
 }
 
-/// Perform or single iteration of pair insertions
-fn single_pair_insertion_process(template: String, rules: &InsertionRules) -> String {
-    template
-        .chars()
-        .zip(template.chars().skip(1))
-        .flat_map(|pair| {
-            if let Some(c) = rules.get(&pair) {
-                vec![pair.0, *c]
-            } else {
-                vec![pair.0]
-            }
-        })
-        .chain(template.chars().rev().take(1))
-        .join("")
+/// Perform a single iteration of pair insertions.
+fn perform_single_pair_insertion_iteration(
+    lc: &mut LetterCounter,
+    cc: &mut ConnectionCounter,
+    conns: &Connections,
+) {
+    let cc_clone = cc.clone();
+
+    conns.iter().for_each(|(&conn, &insert)| {
+        let n_conns = if let Some(&n_conns) = cc_clone.get(&conn) {
+            n_conns
+        } else {
+            0
+        };
+        if n_conns > 0 {
+            *cc.entry((conn.0, insert)).or_insert(0) += n_conns;
+            *cc.entry((insert, conn.1)).or_insert(0) += n_conns;
+            *cc.get_mut(&conn).unwrap() -= n_conns;
+            *lc.entry(insert).or_insert(0) += n_conns;
+        }
+    });
 }
 
-/// Compute the difference between the most and least common elements in the polymer after `steps` iterations.
-pub fn day_14_1(data: &[String], steps: usize) -> usize {
-    let (mut template, rules) = parse_input(data);
-
+/// Compute the difference between the counts of the elements appearing the most and the least.
+pub fn day_14(data: &[String], steps: usize) -> usize {
+    let (mut lc, mut cc, conns) = parse_input(data);
     for _ in 0..steps {
-        template = single_pair_insertion_process(template, &rules);
+        perform_single_pair_insertion_iteration(&mut lc, &mut cc, &conns);
     }
 
-    let counts: HashMap<char, _> = template.chars().fold(HashMap::new(), |mut hm, c| {
-        *hm.entry(c).or_insert(0) += 1;
-        hm
-    });
-    let (min, max) = counts.values().minmax().into_option().unwrap();
-
+    let (min, max) = lc.values().minmax().into_option().unwrap();
     max - min
 }
 
@@ -61,8 +74,7 @@ mod tests {
 
     use super::*;
 
-    #[test]
-    fn test_parse_input() {
+    fn get_test_input() -> (LetterCounter, ConnectionCounter, Connections) {
         let data = vec![
             "NNCB".to_string(),
             "".to_string(),
@@ -84,10 +96,18 @@ mod tests {
             "CN -> C".to_string(),
         ];
 
-        let (polymer_template, rules) = parse_input(&data);
-        assert_eq!(polymer_template, "NNCB");
+        parse_input(&data)
+    }
+
+    #[test]
+    fn test_parse_input() {
+        let (lc, cc, conns) = get_test_input();
+        assert_eq!(lc[&'N'], 2);
+        assert_eq!(lc[&'C'], 1);
+        assert_eq!(lc[&'B'], 1);
+
         assert_eq!(
-            rules,
+            conns,
             [
                 (('C', 'H'), 'B'),
                 (('H', 'H'), 'N'),
@@ -108,38 +128,35 @@ mod tests {
             ]
             .into_iter()
             .collect()
-        )
+        );
+
+        assert_eq!(cc[&('N', 'N')], 1);
+        assert_eq!(cc[&('N', 'C')], 1);
+        assert_eq!(cc[&('C', 'B')], 1);
+        assert!(cc.get(&('A', 'A')).is_none());
     }
 
     #[test]
-    fn test_single_pair_insertion_process() {
-        let data = vec![
-            "NNCB".to_string(),
-            "".to_string(),
-            "CH -> B".to_string(),
-            "HH -> N".to_string(),
-            "CB -> H".to_string(),
-            "NH -> C".to_string(),
-            "HB -> C".to_string(),
-            "HC -> B".to_string(),
-            "HN -> C".to_string(),
-            "NN -> C".to_string(),
-            "BH -> H".to_string(),
-            "NC -> B".to_string(),
-            "NB -> B".to_string(),
-            "BN -> B".to_string(),
-            "BB -> N".to_string(),
-            "BC -> B".to_string(),
-            "CC -> N".to_string(),
-            "CN -> C".to_string(),
-        ];
+    fn test_perform_single_pair_insertion_iteration() {
+        let (mut lc, mut cc, conns) = get_test_input();
 
-        let (polymer_template, rules) = parse_input(&data);
+        perform_single_pair_insertion_iteration(&mut lc, &mut cc, &conns);
 
-        assert_eq!(
-            single_pair_insertion_process(polymer_template, &rules),
-            "NCNBCHB".to_string()
-        );
+        assert_eq!(lc[&'N'], 2);
+        assert_eq!(lc[&'C'], 2);
+        assert_eq!(lc[&'B'], 2);
+        assert_eq!(lc[&'H'], 1);
+
+        // Content at start: NNCB
+        // Content after 1 iteration: NCNBCHB
+        assert_eq!(cc[&('N', 'N')], 0);
+        assert_eq!(cc[&('C', 'B')], 0);
+        assert_eq!(cc[&('N', 'C')], 1);
+        assert_eq!(cc[&('C', 'N')], 1);
+        assert_eq!(cc[&('N', 'B')], 1);
+        assert_eq!(cc[&('B', 'C')], 1);
+        assert_eq!(cc[&('C', 'H')], 1);
+        assert_eq!(cc[&('H', 'B')], 1);
     }
 
     #[test]
@@ -164,7 +181,31 @@ mod tests {
             "CC -> N".to_string(),
             "CN -> C".to_string(),
         ];
+        assert_eq!(day_14(&data, 10), 1588);
+    }
 
-        assert_eq!(day_14_1(&data, 10), 1588);
+    #[test]
+    fn test_day_14_2() {
+        let data = vec![
+            "NNCB".to_string(),
+            "".to_string(),
+            "CH -> B".to_string(),
+            "HH -> N".to_string(),
+            "CB -> H".to_string(),
+            "NH -> C".to_string(),
+            "HB -> C".to_string(),
+            "HC -> B".to_string(),
+            "HN -> C".to_string(),
+            "NN -> C".to_string(),
+            "BH -> H".to_string(),
+            "NC -> B".to_string(),
+            "NB -> B".to_string(),
+            "BN -> B".to_string(),
+            "BB -> N".to_string(),
+            "BC -> B".to_string(),
+            "CC -> N".to_string(),
+            "CN -> C".to_string(),
+        ];
+        assert_eq!(day_14(&data, 40), 2188189693529);
     }
 }
--- tests/day_14.rs
+++ tests/day_14.rs
@@ -1,8 +1,10 @@
 use aoc2021::{self, read_data};
 
 #[test]
-fn test_day_10() {
+fn test_day_14() {
     let data = read_data::<String, _>("data/day14.txt").unwrap();
-    let task_1 = aoc2021::day_14::day_14_1(&data, 10);
+    let task_1 = aoc2021::day_14::day_14(&data, 10);
     assert_eq!(task_1, 5656);
+    let task_2 = aoc2021::day_14::day_14(&data, 40);
+    assert_eq!(task_2, 12271437788530);
 }
